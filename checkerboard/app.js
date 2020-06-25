ec4d5cc9e331d861a554004b537482f6183c861d
Connor <connorskorburg@gmail.com>
2020-06-25 14:03:15 -0500

    completed todo list practice from tutorial

---
+++ checkerboard/app.js
@@ -0,0 +1,14 @@
+function CheckerBoard (props) {
+    return 
+  }
+
+function Cell (props) {
+    return /*Some UI*/
+}
+
+var styles = {
+    row: {height: '20px'},
+    cell: {height: '20px', width: '20px', display:'inline-block'},
+    colorA: {backgroundColor: 'black'},
+    colorB: {backgroundColor: 'red'}
+  }
---
+++ todolist/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import {v4 as uuidv4} from 'uuid';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuidv4(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuidv4(),
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: uuidv4(),
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]});
+  }
+  render(){
+      return (
+        <div className="App">
+          <div className="container">
+            <Header />
+            <AddTodo addTodo = {this.addTodo} />
+            <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo = {this.delTodo} />
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
